bc08c911cdbcf6c311db82e72d26b3e66b0d37b5
Jaehwan Kim <jhwnkim@mit.edu>
2021-05-11 10:50:52 -0400

    Cleaned up data directory and added mapper scripts

--- data-import.sh
+++ data-import.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Loading required module and activate environment
-source /etc/profile
+#source /etc/profile
 
-while getopts f: flag
-do
-	case "${flag}" in 
-		f) infile=${OPTARG};;
-	esac
-done
+# while getopts f: flag
+# do
+# 	case "${flag}" in 
+# 		f) infile=${OPTARG};;
+# 	esac
+# done
 
 # run data preprocess script
 # python data-import-fasta.py
 #python data-import-muscle.py $infile
-python data-import-mpi.py $infile
+#python data-import-mpi.py $infile
+python data-import-mpi.py $1
